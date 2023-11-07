f68a365a56da03216d3cb7f6e8f66a9515dccd36
TuanAnh1003 <anhaanh2003@gmail.com>
2023-11-07 23:45:57 +0700

    feat: service

---
+++ helpers/helper.py
@@ -0,0 +1,56 @@
+from PIL import Image, ImageDraw
+import io
+from io import BytesIO
+def is_center_inside_rectangle(rectangle1, rectangle2):
+    center_x1 = (rectangle1[0] + rectangle1[2]) / 2
+    center_y1 = (rectangle1[1] + rectangle1[3]) / 2
+
+    x2, y2, x3, y3 = rectangle2
+    if x2 < center_x1 < x3 and y2 < center_y1 < y3:
+        return True
+    else:
+        return False
+
+
+def convert_to_coordinates(points):
+    x_values = [point["x"] for point in points]
+    y_values = [point["y"] for point in points]
+
+    xmin = min(x_values)
+    ymin = min(y_values)
+    xmax = max(x_values)
+    ymax = max(y_values)
+
+    coordinates = [xmin, ymin, xmax, ymax]
+    return coordinates
+
+def draw_image_and_boxes(image_file, boxes):
+    """Draw bounding boxes on the image and return the modified image."""
+    img = Image.open(image_file.stream).copy()
+    draw = ImageDraw.Draw(img)
+
+    for box in boxes:
+        print(box)
+        x1, y1, x2, y2 = box["cords"]
+        label = box["class"]
+        probability = box["conf"]
+        draw.rectangle([x1, y1, x2, y2], outline="#00FF00", width=3)
+        text = f"{label} ({probability})"
+        draw.rectangle([x1, y1, x1 + len(text) * 8, y1 + 25], fill="#00ff00")  # Adjust the width based on the text length
+        draw.text((x1, y1 + 18), text, fill="#000000")
+
+    # Save the modified image as bytes and return
+    img_bytes = io.BytesIO()
+    img.save(img_bytes, format='JPEG')
+    img_bytes.seek(0)
+    return img_bytes
+
+
+def crop_image(image_file, coordinates, c):
+    print("Crop image ----------------------->:" + c)
+    image = Image.open(image_file)
+    left, upper, right, lower = coordinates
+    cropped_image = image.crop((left, upper, right, lower))
+    
+    return cropped_image
+
--- services/DetectService.py
+++ services/DetectService.py
@@ -1,46 +1,17 @@
-import os
 from ultralytics import YOLO
 
-
-def is_center_inside_rectangle(obj, rectangle2):
-    rectangle1 = [int(obj[0]), int(obj[1]), int(obj[2]), int(obj[3])]
-    center_x1 = (rectangle1[0] + rectangle1[2]) / 2
-    center_y1 = (rectangle1[1] + rectangle1[3]) / 2
-
-    x2, y2, x3, y3 = rectangle2
-    if x2 < center_x1 < x3 and y2 < center_y1 < y3:
-        return True
-    else:
-        return False
-
-
-def convert_to_coordinates(points):
-    x_values = [point["x"] for point in points]
-    y_values = [point["y"] for point in points]
-
-    xmin = min(x_values)
-    ymin = min(y_values)
-    xmax = max(x_values)
-    ymax = max(y_values)
-
-    coordinates = [xmin, ymin, xmax, ymax]
-    return coordinates
-
-
 class YoloDetect():
-    def __init__(self, detect_class=['car', 'motorcycle']):
+    def __init__(self, model_type, detect_class=['car', 'motorcycle']):
         print('Loading model...')
         self.detect_class = detect_class
-        # Lấy đường dẫn đến thư mục hiện tại của file .py
-        current_directory = os.path.dirname(os.path.abspath(__file__))
-        modal_path = os.path.join(
-            current_directory, 'modals', 'yolov8s', 'last.pt')
-        self.modal = YOLO(modal_path)
+        self.model_type = model_type
+        model_path = "../models/vehicle_detect/last.pt" if model_type == 1 else "../models/lisence_plate_detect/last.pt"
+        self.model = YOLO(model_path)
         print('Model loaded!!')
 
     def detect(self, image):
-        arr = []
-        results = self.modal.predict(image)
+        arr_vehicle = []
+        results = self.model.predict(image)
         for result in results:
             for box in result.boxes:
                 conf = round(box.conf[0].item(), 2)
@@ -49,14 +20,9 @@ class YoloDetect():
                     class_id = result.names[box.cls[0].item()]
                     cords = box.xyxy[0].tolist()
                     cords = [round(x) for x in cords]
-                    arr.append([x1, y1, x2, y2, class_id, conf])
-        return arr
+                    if self.model_type == 1:
+                        arr_vehicle.append({"cords": [x1, y1, x2, y2], "class": class_id, "conf": conf})
+                    else:
+                        return {"cords": [x1, y1, x2, y2], "class": class_id, "conf": conf}
+        return arr_vehicle
 
-    def check_violations(self, image, camera_coordinates):
-        results = self.detect(image)
-        list_vehicle_violation = []
-        camera_rectangle = convert_to_coordinates(camera_coordinates)
-        for obj in results:
-            if not is_center_inside_rectangle(obj, camera_rectangle):
-                list_vehicle_violation.append(obj)
-        return list_vehicle_violation
